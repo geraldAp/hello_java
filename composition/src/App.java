2e2b68ea868fb978263ccd73b5234fcf6ffda2d7
geraldAp <amankwahprincegerald19@gmail.com>
2025-04-10 14:58:59 +0000

    revised composition

---
+++ composition/src/App.java
@@ -0,0 +1,9 @@
+public class App {
+/*
+Composition is a has a relationship un like with an inheritance which is a "is a" relationship
+*/ 
+
+    public static void main(String[] args) throws Exception {
+        System.out.println("Hello, World!");
+    }
+}
---
+++ composition/src/Car.java
@@ -0,0 +1,6 @@
+public class Car {
+    //  compositions having classes as instance variables 
+    private Engine engine;
+    private Tyre[] tyres;
+    private Door[] doors;
+}
