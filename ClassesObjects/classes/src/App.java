caa7f95ccc327e7679f4650e453bbb62511d93b8
geraldAp <amankwahprincegerald19@gmail.com>
2025-04-14 16:42:46 +0000

    - java todo api

--- ClassesObjects/classes/src/App.java
+++ ClassesObjects/classes/src/App.java
@@ -1,8 +1,8 @@
 public class App {
     public static void main(String[] args) throws Exception {
         // creating the object
-        Student student1 = new Student(10);
-        Student studentwithName1 = new Student(10,"Chris");
+        Student student1 = new Student(10, null, null);
+        Student studentwithName1 = new Student(10,"Chris", null);
         
         System.out.println("Hello, World!");
     }
