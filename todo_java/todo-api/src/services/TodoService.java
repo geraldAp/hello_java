445ead5a9460066380a8b7ae05651e25d46daf8f
geraldAp <amankwahprincegerald19@gmail.com>
2025-04-16 17:15:31 +0000

    feat: implement database operations in DBHelper and TodoService
    Add methods in DBHelper to execute queries and updates, and implement CRUD operations in TodoService using DBHelper. This allows interaction with the database for managing todos.

--- todo_java/todo-api/src/services/TodoService.java
+++ todo_java/todo-api/src/services/TodoService.java
@@ -1,28 +1,88 @@
 package services;
 
+import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+
+import utilities.DBHelper;
 
 public class TodoService {
 
     public static List<Map<String, Object>> getTodos() {
 
+        String qry = "SELECT * FROM todo";
+
+        return DBHelper.executeQuery(qry, rs -> {
+            List<Map<String, Object>> todos = new ArrayList<>();
+            try {
+
+                while (rs.next()) {
+                    Map<String, Object> todo = new HashMap<>();
+
+                    todo.put("id", rs.getInt("id"));
+                    todo.put("name", rs.getString("name"));
+                    todo.put("priority", rs.getString("priority"));
+                    todos.add(todo);
+                }
+
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+
+            return todos;
+        });
+
     }
 
     public static Map<String, Object> getTodo(int id) {
-
+        return DBHelper.executeQuery("SELECT * FROM todos WHERE id = " + id, rs -> {
+            try {
+                if (rs.next()) {
+                    Map<String, Object> todo = new HashMap<>();
+                    todo.put("id", rs.getInt("id"));
+                    todo.put("name", rs.getString("name"));
+                    todo.put("priority", rs.getString("priority"));
+                    return todo;
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            return null;
+        });
     }
 
     public static Map<String, Object> addTodo(int id, String name, String priority) {
 
+        String sql = "INSERT INTO todo (name,priority) values (?,?)";
+        int generatedId = DBHelper.executeUpdate(sql, stmt -> {
+            stmt.setString(1, name);
+            stmt.setString(2, priority);
+        });
+        if (generatedId != -1) {
+            return getTodo(generatedId);
+        }
+        return null;
     }
 
-    public static Map<String, Object> updateTodo(int id) {
+    public static Map<String, Object> updateTodoStat(int id, Boolean is_completed, String name, String priority) {
 
+        String sql = "UPDATE todo SET is_completed = ? , name = ? , priority = ?  WHERE id = ? ";
+
+        DBHelper.executeUpdate(sql, stmt -> {
+            stmt.setBoolean(1, is_completed);
+            stmt.setString(2, name);
+            stmt.setString(3, priority);
+            stmt.setInt(4, id);
+        });
+
+        return getTodo(id);
     }
 
     public static Boolean deleteTodo(int id) {
-
+        String stmt = "DELETE FROM todo WHERE id = ?";
+        int affected =  DBHelper.executeUpdate(stmt, null);
+        return affected > 0;
     }
 
 }
--- todo_java/todo-api/src/utilities/DBHelper.java
+++ todo_java/todo-api/src/utilities/DBHelper.java
@@ -1,10 +1,50 @@
 package utilities;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.function.Function;
+
+import db.DBConnection;
 
 public class DBHelper {
     public static <T> T executeQuery(String sql, Function<ResultSet, T> handler) {
 
+        try (Connection conn = DBConnection.getConnection();
+                Statement stmt = conn.createStatement();
+                ResultSet rs = stmt.executeQuery(sql)) {
+
+            return handler.apply(rs);
+
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    public static int executeUpdate(String sql, SQLConsumer<PreparedStatement> binder) {
+        try (Connection conn = DBConnection.getConnection();
+                PreparedStatement stmt = conn.prepareStatement(sql, Statement.RETURN_GENERATED_KEYS)) {
+            binder.accept(stmt);
+            int rowsAffected = stmt.executeUpdate();
+            // if this is an insert , return generated id
+            try (ResultSet keys = stmt.getGeneratedKeys()) {
+                if (keys.next()) {
+                    return keys.getInt(1);
+                }
+            }
+
+            return rowsAffected;
+        } catch (SQLException e) {
+            e.printStackTrace();
+            return -1;
+        }
+    }
+
+    @FunctionalInterface
+    public interface SQLConsumer<T> {
+        void accept(T t) throws SQLException;
     }
 }
